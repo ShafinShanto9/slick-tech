540117f1d238cb15679df1d56ac66b528c8d665e
shafin_shant0 <shafinshanto9@gmail.com>
2022-08-29 17:02:10 +0600

    header design complete

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
-
+import {SearchIcon, ShoppingBagIcon, UserIcon} from '@heroicons/react/outline'
 const Header = () => {
+    const session = false
   return (
     <header className='sticky flex justify-between items-center w-full top-0 z-30 bg-[#E7ECEE] p-4'>
         <div className='flex justify-center items-center md:w-1/5'>
@@ -16,6 +17,31 @@ const Header = () => {
             <a className='headerLink' href="">Explore</a>
             <a className='headerLink' href="">Support</a>
             <a className='headerLink' href="">Bussiness</a>  
+        </div>
+          
+        <div className='flex items-center justify-center gap-x-4 w-1/5'>
+            <SearchIcon className='headerIcon' />
+              <Link href='/checkout'>
+                <div className='relative cursor-pointer'>
+                    <span className='absolute -right-1 -top-1 z-50 h-4 w-4 flex items-center justify-center rounded-full bg-gradient-to-r from-pink-400 to-violet-400 text-white text-[10px]'>5</span> 
+                    <ShoppingBagIcon className='headerIcon' />  
+                </div>
+              </Link>  
+
+              {session ? (
+          <Image
+            src={
+              "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+            }
+            alt=""
+            className="cursor-pointer rounded-full"
+            width={34}
+            height={34}
+            
+          />
+        ) : (
+          <UserIcon className="headerIcon"  />
+        )}
         </div>
 
     </header>
